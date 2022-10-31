50cc81837fa6e12c546c8218d9c91ef0a4cbb856
anil chauhan <anilchauhan3318@gmail.com>
2022-11-01 00:37:11 +0530

    WIP

--- assets/js/custom_js/custom.js
+++ assets/js/custom_js/custom.js
@@ -32,4 +32,65 @@ sections.forEach((sct, i) => {
     toggleClass: {targets: sct, className: "active"}
   });
 })
-.to(sections, { xPercent: -100 * (sections.length), duration:1, ease: "none"})
+// .to(sections, { xPercent: -100 * (sections.length), duration:1, ease: "none"})
+
+gsap.to(".section", {
+  scrollTrigger: {
+    scrub: true
+  }, 
+  y: (i, target) => -ScrollTrigger.maxScroll(window) * target.dataset.speed,
+  ease: "none"
+})
+
+
+
+var tl =gsap.timeline();
+tl
+  .from(".banner_wrapper h5",{
+    onStart:function(){
+      $('.banner_wrapper h5').textillate({ 
+        in: { effect: 'fadeInUp',
+        // callback: function(){
+        //   $('.banner_wrapper h5').textillate('out')
+        // }
+      
+      },
+        // out:  {effect: 'fadeOutUp' }
+      
+      });
+    }
+  })
+
+  .from(".banner_wrapper h1",{
+    opacity:0,
+    delay:1,
+    onStart:function(){
+      $('.banner_wrapper h1').textillate({ 
+        in: { effect: 'fadeInUp',
+        // callback: function(){
+        //   $('.banner_wrapper h1').textillate('out')
+        // }
+      
+      },
+        // out:  {effect: 'fadeOutUp' }
+      
+      });
+    }
+  })
+  .from(".more_content_text h3",{
+   
+    delay:1,
+    onStart:function(){
+      $('.more_content_text h3').textillate({ 
+        in: { effect: 'rollIN',
+        // callback: function(){
+        //   $('.banner_wrapper h1').textillate('out')
+        // }
+      
+      },
+        // out:  {effect: 'fadeOutUp' }
+      
+      });
+    }
+  })
+  
