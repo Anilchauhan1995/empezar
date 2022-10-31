a25b90372b8432c660347a1527714e915ac43655
Anilchauhan1995 <poojavargla1995@gmail.com>
2022-10-31 11:20:40 +0530

    second commit

--- assets/js/custom_js/dist/custom.dev.js
+++ assets/js/custom_js/dist/custom.dev.js
@@ -1,17 +1,11 @@
 // gsap.registerPlugin(ScrollTrigger);
-
 // const sec_scroll = gsap.timeline();
-
 // sec_scroll.to(".main_wrapper",{x:-window.innerWidth})
-
 // ScrollTrigger.create({
 //   animation:sec_scroll,
 //   trigger:"main_wrapper",
 // })
-
-
 // let sections = gsap.utils.toArray(".section");
-
 // gsap.to(sections, {
 //   xPercent: -100 * (sections.length - 1),
 //   scrollTrigger: {
@@ -22,3 +16,4 @@
 //     end: "+=3500",
 //   }
 // });
+"use strict";
