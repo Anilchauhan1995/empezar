8bcb59202ed03220715a218ede4680ffbc6bd686
Anilchauhan1995 <poojavargla1995@gmail.com>
2022-11-01 11:40:33 +0530

    site loder

--- assets/js/custom_js/custom.js
+++ assets/js/custom_js/custom.js
@@ -1,5 +1,39 @@
 gsap.registerPlugin(ScrollTrigger);
 
+
+gsap.fromTo('.viewer',
+{opacity:1
+},
+{
+  opacity:0,
+  duration:2,
+  delay:3.5
+}
+  );
+
+gsap.fromTo('.loading_page',
+{opacity:1},
+{
+  opacity:0,
+  duration:1.5,
+  delay:2.8
+}
+  );
+
+
+  gsap.fromTo('.logo_name',{
+    y:50,
+    opacity:0
+  },
+  {
+    y:0,
+    opacity:1,
+    duration:2,
+    delay:0.5
+  }
+  
+  )
+
 var sections = gsap.utils.toArray(".section");
 let maxWidth = 0;
 
